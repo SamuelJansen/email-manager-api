87265f686f60890f2d22bafdd7742cf35c450f44
samuel.jansen <samuel.jansenn@gmail.com>
2022-05-11 01:23:58 -0300

    feature: adding environment configurations

--- api/src/EmailManagerApi.py
+++ api/src/EmailManagerApi.py
@@ -1,15 +1,5 @@
-from flask import render_template
-
 from python_framework import ResourceManager
-from queue_manager_api import QueueManager
 import ModelAssociation
 
 
-app = ResourceManager.initialize(__name__, ModelAssociation.MODEL, managerList=[
-    QueueManager()
-])
-
-
-# @app.route(f'{app.api.baseUrl}')
-# def home():
-#     return render_template('index.html', staticUrl=ResourceManager.getApiStaticUrl(app))
+app = ResourceManager.initialize(__name__, ModelAssociation.MODEL)
--- api/src/config/VoiceClientConfig.py
+++
@@ -1,6 +0,0 @@
-from globals import getGlobalsInstance
-globalsInstance = getGlobalsInstance()
-
-BASE_URL = globalsInstance.getSetting('voice-api.base-url')
-DEFAULT_TIMEOUT_IN_SECONDS = globalsInstance.getSetting('voice-api.default-timeout-in-seconds')
-API_KEY = globalsInstance.getSetting('voice-api.api-key')
